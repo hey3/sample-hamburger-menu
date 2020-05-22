9263316c974f9572ad5a304072a894a72d5c58e0
Hey3 <kohei.oyama@access-company.com>
2020-05-22 12:34:31 +0900

    fix theme color config

--- src/themes/colorTheme.ts
+++ src/themes/colorTheme.ts
@@ -1,41 +1,23 @@
 export type ColorTheme = {
   BACKGROUND: string
-  BASE: string
-  BORDER: string
-  TEXT_MAIN: string
-  TEXT_SUB: string
-  TEXT_DISABLE: string
-  TEXT_LINK: string
-  WARN: string
-  DANGER: string
-  PRIMARY: string
-  SECONDARY: string
+  OPENED: string
+  CLOSED: string
+  MAIN: string
+  TEXT: string
 }
 
 export const baseLight: ColorTheme = {
   BACKGROUND: '#ffffff',
-  BASE: '#efeff4',
-  BORDER: '#000000',
-  TEXT_MAIN: '#1c1c1c',
-  TEXT_SUB: '#737373',
-  TEXT_DISABLE: '#c1c1c1',
-  TEXT_LINK: '#1067ec',
-  WARN: '#ff8800',
-  DANGER: '#f54d36',
-  PRIMARY: '#143cd1',
-  SECONDARY: '#d15314',
+  OPENED: '#cccccc',
+  CLOSED: '#333333',
+  MAIN: '#0d2538',
+  TEXT: '#ffffff'
 }
 
 export const baseDark: ColorTheme = {
   BACKGROUND: '#222639',
-  BASE: '#1b2440',
-  BORDER: '#737475',
-  TEXT_MAIN: '#ffffff',
-  TEXT_SUB: '#b9b9bb',
-  TEXT_DISABLE: '#6a696d',
-  TEXT_LINK: '#046fad',
-  WARN: '#ea7d00',
-  DANGER: '#df3406',
-  PRIMARY: '#7098af',
-  SECONDARY: '#af9670',
+  OPENED: '#737475',
+  CLOSED: '#737475',
+  MAIN: '#efeff4',
+  TEXT: '#000000'
 }
