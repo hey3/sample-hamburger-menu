6ffc4b0cd4c4f1eea7c051b4385ea0d242355aeb
hey3 <oh1kohei@gmail.com>
2020-05-22 12:45:02 +0900

    fix story

--- src/components/HamburgerMenu/HamburgerMenu.stories.tsx
+++ src/components/HamburgerMenu/HamburgerMenu.stories.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import { createGlobalStyle } from 'styled-components'
 import { action } from '@storybook/addon-actions'
 import { Grid } from '@material-ui/core'
 
 import { Burger } from './Burger'
 import { Nav } from './Nav'
+import { Switch } from '@components/Switch'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(props) => props.theme.color.BACKGROUND};
+  }
+`
 
 export default { title: 'Hamburger' }
 
 export const all = () => (
-  <Grid container direction="row" justify="flex-end" alignItems="center">
-    <Burger menuList={sampleMenu} />
-  </Grid>
+  <>
+    <Grid container direction="row" justify="space-between" alignItems="center">
+      <Switch />
+      <Burger menuList={sampleMenu} />
+    </Grid>
+    <GlobalStyle />
+  </>
 )
 
 const sampleMenu: Pick<React.ComponentProps<typeof Nav>, 'menuList'>['menuList'] = [
