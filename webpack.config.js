29a417d092cafd346b4206039713ba4a7800f16b
hey3 <oh1kohei@gmail.com>
2021-06-22 15:47:23 +0900

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,35 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
-const env = process.env.BUILD_MODE
+
+const mode = process.env.BUILD_MODE || 'development'
 
 module.exports = {
-  mode: env || 'development',
-  entry: './src/main.tsx',
-  output: {
-    path: path.resolve(__dirname, './lib'),
-    filename: 'build.js',
+  mode,
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename],
+    },
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    open: true,
+  },
+  devtool: mode === 'production' ? false : 'inline-source-map',
+  entry: {
+    main: {
+      import: ['./src/main.tsx'],
+    },
+  },
+  experiments: {},
+  // FIXME: assetModulesを使うように修正
+  // experiments: {
+  //   asset: true,
+  // },
+  externals: {},
+  externalsPresets: {},
+  infrastructureLogging: {},
   module: {
     rules: [
       {
@@ -16,14 +37,20 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      // FIXME: assetModulesを使うように修正
+      // {
+      //   test: /\.(png|jpg|gif)$/,
+      //   type: 'asset',
+      // },
     ],
   },
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@themes': path.resolve(__dirname, './src/themes'),
-    },
-    extensions: ['.js', '.ts', '.tsx'],
+  node: {},
+  optimization: {},
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'build.js',
+    assetModuleFilename: 'images/[hash][ext]',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -31,9 +58,16 @@ module.exports = {
       filename: 'index.html',
     }),
   ],
-  devtool: env === 'production' ? false : 'inline-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'lib'),
-    historyApiFallback: true,
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@themes': path.resolve(__dirname, './src/themes'),
+    },
+    extensions: ['.js', '.ts', '.tsx'],
   },
+  resolveLoader: {},
+  snapshot: {},
+  stats: {},
+  target: 'web',
+  watchOptions: {},
 }
