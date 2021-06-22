ee6dd66f24742727577c6975908d3a5e726e35a2
hey3 <oh1kohei@gmail.com>
2021-06-22 15:47:32 +0900

    update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,11 @@
 const custom = require('../webpack.config.js')
 
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-a11y',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
-    '@storybook/addon-viewport',
-    'storybook-readme',
-  ],
-  webpackFinal: (config) => {
-    return {
-      ...config,
-      module: { ...config.module, rules: custom.module.rules },
-      resolve: custom.resolve,
-    }
-  },
+  addons: ['@storybook/addon-a11y', '@storybook/addon-essentials', '@storybook/addon-knobs'],
+  stories: ['../src/components/**/*.stories.tsx'],
+  webpackFinal: config => ({
+    ...config,
+    module: { ...config.module, rules: custom.module.rules },
+    resolve: custom.resolve,
+  }),
 }
