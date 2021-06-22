018dee82cab465d10da2452df6847382953806bb
hey3 <oh1kohei@gmail.com>
2021-06-22 15:49:14 +0900

    fix format

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ const custom = require('../webpack.config.js')
 module.exports = {
   addons: ['@storybook/addon-a11y', '@storybook/addon-essentials', '@storybook/addon-knobs'],
   stories: ['../src/components/**/*.stories.tsx'],
-  webpackFinal: config => ({
+  webpackFinal: (config) => ({
     ...config,
     module: { ...config.module, rules: custom.module.rules },
     resolve: custom.resolve,
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { ThemeProvider } from '@themes/ThemeProvider'
 
 export const decorators = [
-  Story => (
+  (Story) => (
     <ThemeProvider>
       <Story />
     </ThemeProvider>
